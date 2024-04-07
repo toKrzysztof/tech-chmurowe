5736cb672aa940f48f0b3b149a8850b52a603346
Krzysztof Torończak <k.toronczak.dev@gmail.com>
2024-04-07 21:02:47 +0200

    dodaje lab3 zad3 skonczone

--- lab03/zad03/server.js
+++ lab03/zad03/server.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(8080, () => {
-  console.log('Server is running on port ' + 8080);
+app.listen(3000, () => {
+  console.log('Server is running on port ' + 3000);
 });
